d6e3dfbd4cf8ce911f3230aba878c8e2ccd90163
aditya <mhetrasaditya@gmail.com>
2025-08-24 23:35:06 +0530

    setting response http status at exception class and disable stacktrace in response

--- src/main/java/dev/aditya/productservice/exceptions/ControllerAdvices.java
+++ src/main/java/dev/aditya/productservice/exceptions/ControllerAdvices.java
@@ -11,21 +11,21 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 public class ControllerAdvices {
 
 //    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(NotFoundException.class)
-    private ResponseEntity<ExceptionDto> handleNotFoundException(NotFoundException e) {
+//    @ExceptionHandler(NotFoundException.class)
+//    private ResponseEntity<ExceptionDto> handleNotFoundException(NotFoundException e) {
+//
+//        return new ResponseEntity(
+//                new ExceptionDto(e.getMessage(), HttpStatus.NOT_FOUND),
+//                HttpStatus.NOT_FOUND
+//        );
+//    }
 
-        return new ResponseEntity(
-                new ExceptionDto(e.getMessage(), HttpStatus.NOT_FOUND),
-                HttpStatus.NOT_FOUND
-        );
-    }
-
-    @ExceptionHandler(Exception.class)
-    private ResponseEntity<String> handleAllExceptions(Exception e) {
-
-        return new ResponseEntity<>(
-                e.getMessage(),
-                HttpStatus.INTERNAL_SERVER_ERROR
-        );
-    }
+//    @ExceptionHandler(Exception.class)
+//    private ResponseEntity<String> handleAllExceptions(Exception e) {
+//
+//        return new ResponseEntity<>(
+//                e.getMessage(),
+//                HttpStatus.INTERNAL_SERVER_ERROR
+//        );
+//    }
 }
